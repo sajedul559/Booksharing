a814ce349af57a48fc4538da6f02d353e0fe210c
unknown <sajedulkhairul@gmail.com>
2021-08-24 23:09:54 +0600

    test

---
+++ database/migrations/2021_08_19_142615_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('amount')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,36 @@
 |
 */
 
+
+// SSLCOMMERZ Start
+Route::get('/example1', 'SslCommerzPaymentController@exampleEasyCheckout');
+Route::get('/example2', 'SslCommerzPaymentController@exampleHostedCheckout');
+Route::get('/pay', 'SslCommerzPaymentController@index');
+Route::get('/pay-via-ajax', 'SslCommerzPaymentController@payViaAjax');
+Route::get('/success', 'SslCommerzPaymentController@success');
+Route::get('/fail', 'SslCommerzPaymentController@fail');
+Route::get('/cancel', 'SslCommerzPaymentController@cancel');
+Route::get('/ipn', 'SslCommerzPaymentController@ipn');
+
+
+
+
+
+
+// Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+// Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+// Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+// Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+// Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+// Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+// Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+// Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
+
+
 Route::get('/', 'PagesController@index')->name('index');
 
 Route::get('/books/search', 'BooksController@search')->name('books.search');
