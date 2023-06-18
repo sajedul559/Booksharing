46e2b831efcaba12687ef0b656dfd1d949a760dc
sajedul559 <sajedulkhairul@gmail.com>
2023-06-18 23:50:25 +0600

    unnecessary code remove from categorycontroller

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -11,13 +11,7 @@ use App\Book;
 class CategoriesController extends Controller
 {
     public function show($slug)
-    {
-        //   $category = Category::where('slug', $slug)->first();
-        //     if (!is_null($category)) {
-        //         $books = $category->books()->orderBy('id', 'desc')->paginate(20);
-        //         return view('frontend.pages.books.index', compact('category', 'books'));
-        //     }
-        //     return redirect()->route('index');  
+    { 
         $category = Category::where('slug', $slug)->first();
         if (!is_null($category)) {
             $books = $category->books()->orderBy('id', 'desc')->paginate(20);
